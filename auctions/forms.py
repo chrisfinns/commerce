80e982cd4ba6b1f5e004d78a669fc605e95e512f
chrisfinns <cf4856@gmail.com>
2023-07-04 22:01:25 +0900

    CreateListing

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,2 +1,27 @@
 from django import forms
 
+
+
+class NewListing(forms.Form):
+    title = forms.CharField(label="Title",widget=forms.TextInput(attrs={
+        'placeholder': 'Name', 
+        'style': 'width: 300px;', 
+        'class': 'form-control'
+        }))
+
+
+
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Please enter the  description',
+        'class': 'form-control',
+
+        }))
+    
+    url = forms.URLField(label="Link to Image", required=False, widget=forms.TextInput(attrs={
+         'class': 'form-control',
+         'style': 'width: 300px;', 
+    }))
+    starting_price = forms.DecimalField(max_digits=14, decimal_places=2, widget=forms.NumberInput(attrs={
+        'class': 'form-control',
+        'style': 'width: 300px;', 
+    }))
